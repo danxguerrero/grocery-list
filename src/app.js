bf89af01d95bc4f1eddcf8f66be19f0a614f7c0c
danxguerrero <danxguerrero@yahoo.com>
2024-02-23 23:41:51 -0500

    Added try/catch to routes for error handling.

--- src/app.js
+++ src/app.js
@@ -6,107 +6,147 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 // User routes
-app.get("/users", async(req, res) => {
-    const allUsers = await User.findAll();
-    res.send(allUsers);
+app.get("/users", async(req, res, next) => {
+    try{
+        const allUsers = await User.findAll();
+        res.send(allUsers);
+    } catch(error) {
+        next(error)
+    }
 });
 
-app.get("/users/:id", async(req, res) => {
-    const id = req.params.id;
-    const user = await User.findByPk(id);
-    res.send(user);
+app.get("/users/:id", async(req, res, next) => {
+    try {
+        const id = req.params.id;
+        const user = await User.findByPk(id);
+        res.send(user);
+    } catch(error) {
+        next(error)
+    }
 });
 
-app.post("/users", async (req, res) => {
-    const createdUser = await User.create(req.body);
-    res.json(createdUser);
+app.post("/users", async (req, res, next) => {
+    try {
+        const createdUser = await User.create(req.body);
+        res.json(createdUser);
+    } catch (error) {
+        next(error)
+    }
 });
 
-app.delete("/users/:id", async (req, res) => {
-    const deleted = await User.destroy({
-        where: {
-            id: req.params.id
-        }
-    });
-    res.sendStatus(200);
+app.delete("/users/:id", async (req, res, next) => {
+    try {    
+        const deleted = await User.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.sendStatus(200);
+    } catch (error) {
+        next(error)
+    }
 });
 
 // List routes
 // Create a list
-app.post("/users/:id/list", async (req, res) => {
-    const createdList = await List.create(req.body);
-    const user = await User.findByPk(req.params.id);
-    await createdList.setUser(user);
-    await createdList.addCollaborators(user);
-    res.json(createdList);
+app.post("/users/:id/list", async (req, res, next) => {
+    try {    
+        const createdList = await List.create(req.body);
+        const user = await User.findByPk(req.params.id);
+        await createdList.setUser(user);
+        await createdList.addCollaborators(user);
+        res.json(createdList);
+    } catch(error) {
+        next(error)
+    }
 });
 
 // Get all lists from created by a user
-app.get("/users/:id/lists", async (req, res) => {
-    const allLists = await List.findAll({
-        include: [
-            {
-                model: User,
-                as: 'collaborators', 
-                attributes: ['id', 'username'],
+app.get("/users/:id/lists", async (req, res, next) => {
+    try {
+        const allLists = await List.findAll({
+            include: [
+                {
+                    model: User,
+                    as: 'collaborators', 
+                    attributes: ['id', 'username'],
+                }
+            ],
+            where: {
+                userId: req.params.id,
             }
-          ],
-        where: {
-            userId: req.params.id,
-        }
-    });
-    res.json(allLists)
+        });
+        res.json(allLists)
+    } catch(error) {
+        next(error)
+    }
 });
 
-app.get("/lists", async (req, res) => {
-    const allLists = await List.findAll({
-        include: [
-            {
-                model: User,
-                as: 'collaborators', 
-                attributes: ['id', 'username'],
-            }
-          ]
-    });
-    res.json(allLists)
+app.get("/lists", async (req, res, next) => {
+    try {    
+        const allLists = await List.findAll({
+            include: [
+                {
+                    model: User,
+                    as: 'collaborators', 
+                    attributes: ['id', 'username'],
+                }
+            ]
+        });
+        res.json(allLists)
+    } catch(error) {
+        next(error)
+    }
 })
 
 // Get a list by id
-app.get("/list/:id", async (req, res) => {
-    const list = await List.findByPk(req.params.id, {
-        include: [
-            {
-                model: User,
-                as: 'collaborators', 
-                attributes: ['id', 'username'],
-            },
-            {
-                model: Item,
-                attributes: ['name', 'createdBy'],
-            }
-          ]
-    })
-    res.json(list)
+app.get("/list/:id", async (req, res, next) => {
+    try {
+        const list = await List.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    as: 'collaborators', 
+                    attributes: ['id', 'username'],
+                },
+                {
+                    model: Item,
+                    attributes: ['name', 'createdBy'],
+                }
+            ]
+        })
+        res.json(list)
+    } catch(error){
+        next(error)
+    }
 });
 
 // Update a list 
-app.put("/users/:userId/list/:listId", async (req, res) => {
-    const list = await List.findOne({
-        where: {
-            id: req.params.listId,
-            userId: req.params.userId
-        }
-    });
-    const updatedList = await list.update(req.body);
-    res.json(updatedList);
+app.put("/users/:userId/list/:listId", async (req, res, next) => {
+    try {    
+        const list = await List.findOne({
+            where: {
+                id: req.params.listId,
+                userId: req.params.userId
+            }
+        });
+        const updatedList = await list.update(req.body);
+        res.json(updatedList);
+    } catch(error) {
+        next(error)
+    }
 });
 
 // Add collaborator to list
-app.post("/list/:listId/collaborator/:userId", async (req, res) => {
-    const list = await List.findByPk(req.params.listId)
-    const user = await User.findByPk(req.params.userId)
-    list.addCollaborators(user)
-    res.sendStatus(201)
+app.post("/list/:listId/collaborator/:userId", async (req, res, next) => {
+    try {    
+        const list = await List.findByPk(req.params.listId)
+        const user = await User.findByPk(req.params.userId)
+        list.addCollaborators(user)
+        res.sendStatus(201)
+    } catch(error) {
+        next(error)
+    }
 });
 
 // Remove collaborator from list
@@ -129,14 +169,18 @@ app.put("/list/:listId/collaborator/:userId", async (req, res) => {
   });
 
 // Delete a list
-app.delete("/users/:userId/list/:listId", async (req, res) => {
-    await List.destroy({
-        where: {
-            id: req.params.listId,
-            userId: req.params.userId
-        }
-    });
-    res.sendStatus(200);
+app.delete("/users/:userId/list/:listId", async (req, res, next) => {
+    try {    
+        await List.destroy({
+            where: {
+                id: req.params.listId,
+                userId: req.params.userId
+            }
+        });
+        res.sendStatus(200);
+    } catch(error) {
+        next(error)
+    }
 });
 
 // Item routes
